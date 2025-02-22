9e998fb88a0d405a355ab2af179e51db57c0244c
Rahul <venkatrahulxyz@gmail.com>
2025-02-22 20:49:11 +0530

    completed minimax algo

--- script.js
+++ script.js
@@ -30,6 +30,15 @@ cellsarray.map((cell)=>{
                 if(!isgameover()){
                     currplayer=(currplayer==="X")?"O":"X";
                     statusMessage.textContent=`Its ${currplayer}'s turn`;
+                    if(currplayer==="O"){
+                        const index=mini();
+                        cellsarray[index].innerText="O";
+                        gamestate[index]="O";
+                        if(!isgameover()){
+                            currplayer="X";
+                            statusMessage.textContent=`Its ${currplayer}'s turn`;
+                        }
+                    }
                 }
             }
         }
@@ -55,8 +64,93 @@ function isgameover() {
             gameactive = false;
             return true;
         }
+        else if(!gamestate.includes("")){
+            statusMessage.textContent = `Its a Draw`;
+            gameactive = false;
+            return true;
+        }
         return false;
     });
 }
 
 
+function mini() {
+    let index = -1;
+    let best = Infinity;
+    for (var i = 0; i < gamestate.length; i++) {
+        if (gamestate[i] === "") {
+            gamestate[i] = "O"; 
+            let temp = maxvalue();
+            if (temp < best) {
+                index = i;
+                best = temp;
+            }
+            gamestate[i] = ""; 
+        }
+    }
+    return index;
+}
+
+function minvalue() {
+    let result = checkWinner();
+    if (result !== null) {
+        return result; 
+    }
+    
+    let best = Infinity;
+    for (var i = 0; i < gamestate.length; i++) {
+        if (gamestate[i] === "") {
+            gamestate[i] = "O"; 
+            best = Math.min(best, maxvalue());
+            gamestate[i] = "";
+        }
+    }
+    return best;
+}
+
+function maxvalue() {
+    let result = checkWinner();
+    if (result !== null) {
+        return result; 
+    }
+    
+    let best = -Infinity;
+    for (var i = 0; i < gamestate.length; i++) {
+        if (gamestate[i] === "") {
+            gamestate[i] = "X"; 
+            best = Math.max(best, minvalue());
+            gamestate[i] = ""; 
+        }
+    }
+    return best;
+}
+
+function utility() {
+    let result = checkWinner();
+    if (result !== null) {
+        let noofemptyspaces = gamestate.filter(cell => cell === "").length;
+        if (result === 1) {
+            return 1 + noofemptyspaces; 
+        } else if (result === -1) {
+            return -1 - noofemptyspaces; 
+        } else {
+            return 0;
+        }
+    }
+    return null; 
+}
+
+function checkWinner() {
+    for (const combi of winningCombinations) {
+        if (gamestate[combi[0]] === "X" && gamestate[combi[1]] === "X" && gamestate[combi[2]] === "X") {
+            return 1;
+        } 
+        if (gamestate[combi[0]] === "O" && gamestate[combi[1]] === "O" && gamestate[combi[2]] === "O") {
+            return -1; 
+        }
+    }
+    if (!gamestate.includes("")) {
+        return 0; 
+    }
+    return null;
+}
